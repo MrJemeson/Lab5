62608cfabfc7dc6050b64376adec555a040dca49
Ilya Andrianov <2005ilyaaa@gmail.com>
2025-09-10 03:54:07 +0300

    lab5

--- src/main/java/ru/bmstu/App.java
+++ src/main/java/ru/bmstu/App.java
@@ -1,5 +1,17 @@
 package ru.bmstu;
 
-public class App {
+import com.opencsv.exceptions.CsvValidationException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.bmstu.components.UI;
+import ru.bmstu.config.AppConfig;
 
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws CsvValidationException, IOException {
+        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class)) {
+            UI ui = context.getBean(UI.class);
+            ui.start();
+        }
+    }
 }
--- src/main/java/ru/bmstu/config/AppConfig.java
+++ src/main/java/ru/bmstu/config/AppConfig.java
@@ -1,13 +1,18 @@
 package ru.bmstu.config;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import org.springframework.core.io.FileSystemResource;
+import ru.bmstu.repositories.UserRepository;
+import ru.bmstu.repositories.impl.CsvUserRepositoryImpl;
 
 @Configuration
 @ComponentScan("ru.bmstu")
+@EnableAspectJAutoProxy
 @PropertySource("classpath:application.properties")
 public class AppConfig {
-
+    @Bean
+    public CsvUserRepositoryImpl CsvUserRepository() {
+        return new CsvUserRepositoryImpl(new FileSystemResource("src/main/resources/data/users.csv"));
+    }
 }
